f2c2339c4804ae17a57ed2b5b47b36821b80ccfc
Atakan Demircioğlu <31544037+atakde@users.noreply.github.com>
2023-04-01 20:07:44 +0300

    cors

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const errorController = require("./controllers/errorController");
+const cors = require("cors");
 const PORT = 3000;
 
 const app = express();
+
+// enable cors for all origins
+app.use(cors());
 
 // Implement body parser
 app.use(bodyParser.json());
